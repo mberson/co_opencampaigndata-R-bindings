bb4e16e56efd15246e02d9d8535cf52876284965
Max Berson <maxberson@gmail.com>
2013-08-26 11:40:35 -0600

    Created controller to run test suite

---
+++ test/run_tests.R
@@ -0,0 +1,11 @@
+library('RUnit')
+
+source('expenditures.R')
+
+test.suite <- defineTestSuite("expenditures",
+                              dirs = file.path("."),
+                              testFileRegexp = '^\\w\\.R')
+
+test.result <- runTestSuite(test.suite)
+
+printTextProtocol(test.result)
