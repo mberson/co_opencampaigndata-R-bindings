fb3c9f4205e93a799036eb633a81bb3004d3c9ec
Max Berson <maxberson@gmail.com>
2013-09-27 19:36:43 -0600

    Listed search params for expenditures

--- test/test_suite.R
+++ test/test_suite.R
@@ -1,5 +1,61 @@
-contribution_req <- list ( apiKey = "XXXXXX" ,
-                           zip    = "80211" ,
-                           minAmount = "600" ,
-                           maxAmount = "601"
-                         )
+library ( 'RCurl' )
+library ( 'rjson' )
+
+# Don't store the API publicly
+
+apiKey <- readline ( 'Please enter your API key: ' )
+
+# A query with no parameters. This will basically check that R is communicating
+# well with the API endpoint.
+
+get_expenditures_no_params <- function () {
+	query	<- paste ( 'http://co.opencampaigndata.org/expenditures?apiKey=' ,
+	                   apiKey ,
+	                   sep="" ,
+	                   collapse=""
+	                 )
+	json	<- getURL ( query )
+	r_data	<- fromJSON ( json )
+	return ( r_data )
+}
+
+# Exhaustively represent an expenditures query
+
+expenditures_query <- list (
+	offset				= '5' ,
+	limit				=   ,
+	commiteeID			=   ,
+	minAmount			=   ,
+	maxAmount			=   ,
+	startDate			=   ,
+	endDate				=   ,
+	lastName			=   ,
+	firstName			=   ,
+	middleInitial		=   ,
+	address				=   ,
+	city				=   ,
+	state				=   ,
+	zip					=   ,
+	recordID			=   ,
+	startFiledDate		=   ,
+	endFiledDate		=   ,
+	expenditureType		=   ,
+	paymentType			=   ,
+	disbursementType	=   ,
+	electioneering		=   ,
+	committeeType		=   ,
+	committeeName		=   ,
+	candidateName		=   ,
+	employer			=   ,
+	occupation			=   ,
+	amended				=   ,
+	amendment			=   ,
+	amendedRecordID		=   ,
+	jurisdiction		=   ,
+	fields				=   
+}
+
+# Test everything
+run_tests <- function () {
+	get_expenditures_no_params()
+}
